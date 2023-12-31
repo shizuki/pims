86d2e2b7dd09cb45e3f98cf234039812d129900f
WID_兼田純 <jun_kaneda@worksid.co.jp>
2023-12-31 18:11:54 +0900

    feat: 初期諸々追加
    - tailwind
    - breeze(LiveWire)
    - lefthook
    - aws-sdk-php
    - Larastan
    - pint
    - 言語ファイル郡

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,6 +56,18 @@ return [
             'throw' => false,
         ],
 
+        'minio' => [
+            'driver' => 's3',
+            'key' => env('MINIO_ACCESS_KEY_ID'),
+            'secret' => env('MINIO_SECRET_ACCESS_KEY'),
+            'region' => env('MINIO_DEFAULT_REGION'),
+            'bucket' => env('MINIO_BUCKET'),
+            'url' => env('MINIO_URL', 'http://localhost:9000/'.'MINIO_BUCKET'),
+            'endpoint' => env('MINIO_ENDPOINT', 'http://localhost:9000'),
+            'use_path_style_endpoint' => true,
+            'throw' => false,
+        ],
+
     ],
 
     /*
