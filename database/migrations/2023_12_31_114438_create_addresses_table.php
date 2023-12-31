6928a8cdbbb0f03f57f695d7893cb9fd83eb7aae
WID_兼田純 <jun_kaneda@worksid.co.jp>
2023-12-31 22:32:45 +0900

    feat: 住所テーブル追加

---
+++ database/migrations/2023_12_31_114438_create_addresses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->comment('ユーザーID');
+            $table->integer('kind')->comment('住所種別');
+            $table->char('zipcode', 7)->comment('郵便番号');
+            $table->char('address', 255)->comment('住所');
+            $table->char('street_number', 255)->comment('丁目・番地・号');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
