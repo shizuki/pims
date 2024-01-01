51ed4073d9ac6f4994fee100f2559d24e1d061c6
WID_兼田純 <jun_kaneda@worksid.co.jp>
2024-01-01 21:03:02 +0900

    change: pint実行
    - 未使用クラスの`use`は一旦コメントアウト
    - `use`並び順変更

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Database\Eloquent\Relations\BelongsTo;
+// use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Address extends Model
 {
@@ -44,7 +44,7 @@ class Address extends Model
         'password' => 'hashed',
     ];
 
-    function user(): HasOne
+    public function user(): HasOne
     {
         return $this->hasOne(User::class, 'user_id');
     }
