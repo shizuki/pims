657ce88dc3f4c978733c2730a9072a2e9e681920
WID_兼田純 <jun_kaneda@worksid.co.jp>
2024-01-01 22:10:58 +0900

    feat: IDEヘルパーインストール
    - 推奨拡張追記
    - DocComment自動生成

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,6 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 // use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+/**
+ * App\Models\Address
+ *
+ * @property int $id
+ * @property int $user_id ユーザーID
+ * @property int $kind 住所種別
+ * @property string $zipcode 郵便番号
+ * @property string $address 住所
+ * @property string $street_number 丁目・番地・号
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @property mixed $password
+ * @property-read \App\Models\User|null $user
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Address newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Address newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Address query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereKind($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereStreetNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Address whereZipcode($value)
+ *
+ * @mixin \Eloquent
+ */
 class Address extends Model
 {
     use HasFactory;
