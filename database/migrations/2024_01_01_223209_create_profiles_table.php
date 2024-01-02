4e20128537540e2bffb484e10fbba7a23a11ff13
WID_兼田純 <jun_kaneda@worksid.co.jp>
2024-01-02 15:36:35 +0900

    change: プロフィールテーブルのマイグレーションにコメント追加

--- database/migrations/2024_01_01_223209_create_profiles_table.php
+++ database/migrations/2024_01_01_223209_create_profiles_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->char('hobby')->nullable();
-            $table->char('blood_type')->nullable();
-            $table->char('special_skill')->nullable();
-            $table->date('birthday')->nullable();
+            $table->integer('user_id')->comment('ユーザーID');
+            $table->char('hobby')->nullable()->comment('趣味');
+            $table->char('blood_type')->nullable()->comment('血液型');
+            $table->char('special_skill')->nullable()->comment('特技');
+            $table->date('birthday')->nullable()->comment('生年月日');
             $table->timestamps();
         });
     }
